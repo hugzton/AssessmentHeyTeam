b2907670a074b99444417fcf93e63c8449299bdf
Hugo Toussaint <hugo.toussaint@epitech.eu>
2020-07-23 14:13:58 +0200

    adjust css, final commit

--- js/script.js
+++ js/script.js
@@ -3,65 +3,99 @@ var values = [
     [195,31,102,148,119,28,190,197,145,190,84,91,62,101,72,178,81,92,119,103,183,57,122,47,170,12,19,193,196,199,47,43,85,152,158,151,42,26,112,44,163,186,50,121,152,49,169,136,198,98], 
     [26,98,102,120,105,153,47,167,188,41,121,109,16,199,83,196,188,101,122,121,193,59,77,27,43,55,70,186,24,118,185,63,122,68,119,0,16,44,181,135,102,43,134,91,180,152,94,169,110,31]];
 
+/**
+ * 
+ * @param {*} arr 
+ * @param {*} val 
+ * Used to find the occurences of value(val) in array(arr), return 0 if not
+ */
 const countOccurrences = (arr, val) => arr.reduce((a, currentValue) => (currentValue === val ? a + 1 : a), 0);
 
+/**
+ * 
+ * @param {*} values 
+ * init a list of object with item(values) given
+ */
 function initMap(values){
     let map = []
-    for(var i = 0; i < values.length; i++){
+    values.forEach((column, i) => {
         map.push([])
-        for(var j = 0; j < values[i].length ; j++){
+        column.forEach((item) => {
             map[i].push({
-              value: values[i][j],
-              occur: 0,
-              color: "white"
-            })
-        }
-    }
+                value: item,
+                occur: 0,
+                color: "white"
+              })
+        })
+    })
     return map
 }
 
+/**
+ * 
+ * @param {*} map 
+ * @param {*} limit 
+ * Used to give green color for all first two value in map (left to right) under the limit
+ */
 function handleGreen(map, limit){
     let occur = []
-    for(var i = 0; i < map.length; i++){
-      for(var j = 0; j < map[i].length ; j++){
-        if (countOccurrences(occur, map[i][j].value) < 2  &&  map[i][j].value > limit) {
-          map[i][j].color = "green"
-        }
-        occur.push(map[i][j].value)
-      }
-    }
+    map.forEach((column) => {
+        column.forEach((item) => {
+            if (countOccurrences(occur, item.value) < 2  &&  item.value > limit) {
+                item.color = "green"
+            }
+            occur.push(item.value)    
+        })
+    }) 
     return map
 }
-  
+
+/**
+ * 
+ * @param {*} map 
+ * @param {*} limit 
+ * Used to give red color for first value in map (right to left) under the limit
+ */
 function handleRed(map, limit) {
     let occur = []
-    for(var i = map.length - 1; i >= 0; i--){
-      for(var j = 0; j < map[i].length; j++){
-        if (countOccurrences(occur, map[i][j].value) < 1  &&  map[i][j].value < limit) {
-          map[i][j].color = "red"
-        }
-        occur.push(map[i][j].value)
-      }
-    }
+    map.slice().reverse()
+	    .forEach((column) => {
+			column.forEach((item) => {
+                if (countOccurrences(occur, item.value) < 1  &&  item.value < limit) {
+                    item.color = "red"
+                }
+                occur.push(item.value)        
+            })
+	})
     return map
 }
-  
+
+/**
+ * 
+ * @param {*} map 
+ * Used to handle color with handleRed & handleGreen method
+ */
 function handleColors(map){
     return handleRed(handleGreen(map, 100), 100)
 }
 
+/**
+ * 
+ * @param {*} values 
+ * Used to generate html DOM table with map(values) given
+ */
 function createTable(values){
     var body = document.body,
         tbl  = document.createElement('table');
 
-    for(var i = 0; i < values.length; i++){
-        var tr = tbl.insertRow();
-        for(var j = 0; j < values[i].length ; j++){
-          var td = tr.insertCell();
-          td.appendChild(document.createTextNode(values[i][j].value));
-          td.style.backgroundColor = values[i][j].color;
-        }
-    }
+    values.forEach((column) => {
+        var tr = tbl.insertRow()
+        column.forEach((item) => {
+          var td = tr.insertCell()
+          td.appendChild(document.createTextNode(item.value))
+          td.style.backgroundColor = item.color
+        })
+    })
     body.appendChild(tbl);
 }
 
